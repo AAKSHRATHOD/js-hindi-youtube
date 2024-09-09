cc214cc200b05e886ae1aa13885092a6bd9c79ef
AAKSHRATHOD <99034938+AAKSHRATHOD@users.noreply.github.com>
2024-09-09 16:01:27 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,3 +2,20 @@
 // alert(3+3) // were using node js,not browser
 console.log(3+2)
 console.log("aakash")
+
+let name= "aakash"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number ... 2 to power 53
+// bigint
+// string... " "
+// boolean .... true/false
+// null 
+// undefined .... 
+// symbol.... unique
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);

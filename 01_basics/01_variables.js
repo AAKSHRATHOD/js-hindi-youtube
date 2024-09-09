6e6ce3b7247ab9d7a1c422c558a22f4df64ba125
AAKSHRATHOD <99034938+AAKSHRATHOD@users.noreply.github.com>
2024-09-09 15:36:25 +0000

    nwe file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId =144553
+let accountEmail ="aakash@google.com"
+var accountPassword ="12345"
+accountCity = "jaipur"
+let accountState;
+// accountId =2 // not allowed
+
+
+
+accountEmail ="hc@hc.com"
+accountPassword="212121"
+accountCity="ahemdabad"
+console.log(accountId)
+/*
+prefer not to use var
+because of isssue in block scope and functional scope 
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
